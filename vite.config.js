f26d1b5ffebfd615ae3715c23b24ca19d9ee14d3
Joseph Liu <drowsy.liu@gmail.com>
2025-07-21 11:46:18 +0800

    fix base to project name

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/nodejs/vite-router/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/nodejs/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
