3270dd4897d2566f325296fcf010320f790d856c
Joseph Liu <josephliu@digiwin.com>
2025-07-30 21:56:30 +0800

    update current

---
+++ airebnb-ssr/src/views/home/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+<template>
+    <div>
+        首頁
+    </div>
+</template>
+<style>
+</style>
---
+++ airebnb-ssr/src/views/mine/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+<template>
+    <div>
+        個人中心
+    </div>
+</template>
+<style>
+</style>
