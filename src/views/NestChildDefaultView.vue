1fd6afee307f407b5d1d83ea22a39a0a2cc529c7
Joseph Liu <josephliu@digiwin.com>
2025-07-30 23:00:56 +0800

    Updates

--- src/views/NestChildDefaultView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Nest Child Default View</h1>
-    </div>
-</template>
--- src/views/NestChildView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="nest-view">
-        <h2>nest child routing inner</h2>
-    </div>
-</template>
