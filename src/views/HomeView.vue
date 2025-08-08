d716880f315313b18e93448a73bd4873b02e38de
Joseph Liu <josephliu@digiwin.com>
2025-08-09 01:15:25 +0800

    added link to Order view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,8 +6,9 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
      <div class="Home">
-    <h1>This is an Home page</h1>
-  </div>
+      <h1>Please go to Order</h1>
+      <H1><router-link to="/order">Go to Order</router-link></H1>
+    </div>
     <TheWelcome />
   </main>
 </template>
